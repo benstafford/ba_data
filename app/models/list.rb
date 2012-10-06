6bb26a6b9fac784ae80a31f136bb683d39bf4d86
Ben Stafford <bendstafford@gmail.com>
2012-10-06 11:04:43 -0400

    Clean up syntax a bit - no need to be as verbose

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,15 +2,15 @@ class List
   include ::BeerAdvocate
 
   def self.all
-    response = BeerAdvocate::fetch("/lists/popular")
     #query to get <a> tags for all beer lists
-    results = response.search("//h3[text()='Lists']/following-sibling::li/a")
+    BeerAdvocate.fetch("/lists/popular").
+      search("//h3[text()='Lists']/following-sibling::li/a")
 
     #each[:href] and each.content
   end
 
   def self.find(list)
-    response = BeerAdvocate::fetch("/lists/" + list)
-    results = response.search("//a[contains(@href, '/beer/profile')]")
+    BeerAdvocate::fetch("/lists/" + list).
+      search("//a[contains(@href, '/beer/profile')]")
   end
 end
