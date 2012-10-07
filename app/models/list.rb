31c181753bda50ec78946dc816098baa7b2edff9
Ben Stafford <bendstafford@gmail.com>
2012-10-06 22:04:13 -0400

    Extra slash was causing unpredictable results

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,14 +3,14 @@ class List
 
   def self.all
     #get all lists published on the site
-    BeerAdvocate.fetch("/lists/popular").
+    BeerAdvocate.fetch("lists/popular").
       search("//h3[text()='Lists']/following-sibling::li/a").
       map { |item| { :id => item[:href].gsub(/\/lists\//, ""), :name => item.content} }
   end
 
   def self.find(list)
     #get all beers in a particular list
-    BeerAdvocate::fetch("/lists/" + list).
+    BeerAdvocate.fetch("lists/" + list).
       search("//a[contains(@href, '/beer/profile')]/b/..").
       map { |item| { :id => parameterize_profile_url(item[:href]), :name => item.content} }
   end
