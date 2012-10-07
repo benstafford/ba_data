e3780b48331013edea2a6cfaa824346b2181d3e7
Ben Stafford <bendstafford@gmail.com>
2012-10-06 21:30:53 -0400

    Rename returned results a bit

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,14 +5,14 @@ class List
     #get all lists published on the site
     BeerAdvocate.fetch("/lists/popular").
       search("//h3[text()='Lists']/following-sibling::li/a").
-      map { |item| { :href => item[:href], :name => item.content} }
+      map { |item| { :id => item[:href].gsub(/\/lists\//, ""), :name => item.content} }
   end
 
   def self.find(list)
     #get all beers in a particular list
     BeerAdvocate::fetch("/lists/" + list).
       search("//a[contains(@href, '/beer/profile')]/b/..").
-      map { |item| { :href => parameterize_profile_url(item[:href]), :name => item.content} }
+      map { |item| { :id => parameterize_profile_url(item[:href]), :name => item.content} }
   end
 
   def self.parameterize_profile_url(full_url)
