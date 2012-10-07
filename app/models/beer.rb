ab2d46264a8ce8720ddce4ce2cadea70015f6efb
Ben Stafford <bendstafford@gmail.com>
2012-10-06 22:05:03 -0400

    Fix syntax

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,7 +3,7 @@ class Beer
 
   def self.find(profile_path)
     #get all beers in a particular list
-    response = BeerAdvocate::fetch("beer/profile/" + profile_path.gsub(/-/, "/"))
+    response = BeerAdvocate.fetch("beer/profile/" + profile_path.gsub(/-/, "/"))
 
     beer_name = response.search("//h1").text
     reviewers_rating = response.search("//td[text()[contains(., 'BA SCORE')]]/span").text
